8cb2a4bae80e64ce4ddebd6323b8e23df32fce10
Victoria Iurina <Victoria_iurina@epam.com>
2021-03-09 20:16:37 +0300

    feat: Add solution for 'Towel Sort'

--- src/index.js
+++ src/index.js
@@ -2,5 +2,16 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if (matrix === undefined || matrix.length === 0) {
+    return [];
+  } else {
+    let newArr = [];
+    matrix.forEach(function (item, index) {
+      if(index % 2 !== 0) {
+        item.reverse();
+      }
+      newArr = newArr.concat(item);
+    });
+    return newArr;
+  }
 }
